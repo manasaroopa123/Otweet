3e2593e3676aba681b64fa2c9959669ccb8aa9d4
manasaroopa123 <158608143+manasaroopa123@users.noreply.github.com>
2025-08-24 17:56:30 +0530

    Add files via upload

---
+++ src/main/java/com/otweet/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.otweet.repository;
+
+import com.otweet.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
+}
