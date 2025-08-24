5f6f0360921905eb3cddd4f8f6404144ac74bb89
manasaroopa123 <158608143+manasaroopa123@users.noreply.github.com>
2025-08-24 18:07:34 +0530

    Add files via upload

---
+++ src/main/java/com/otweet/OtweetApplication.java
@@ -0,0 +1,11 @@
+package com.otweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtweetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OtweetApplication.class, args);
+    }
+}
